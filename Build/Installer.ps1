7f5a3fa6faab6ff49f761c3738b90c624d6fe290
apobekiaris <apostolis.bekiaris@gmail.com>
2017-09-11 12:52:40 +0300

    Failed to build downloable sources
    http://www.expandframework.com/forum/5-installation-configuration-a-updates/5071-failed-to-build-xpand-source.html

--- Build/Installer.ps1
+++ Build/Installer.ps1
@@ -33,7 +33,7 @@ Copy-Item ".\Xpand.DLL\PLugins\Xpand.VSIX.vsix" "$packageFolder\\Xpand.VSIX-$DXV
 $sourceFolder="$installerFolder\Source\"
 Get-ChildItem $XpandFolder -recurse -Include "*.*" |where{
     $fullName=$_.FullName
-    ("*\Build\Installer*","*\Build\_Package*", "*\.git\*",'*\$RECYCLE.BIN\*',"*\System Volume Information\*","*\_third_party_assemblies\*",
+    ("*\Build\Installer*","*\Build\_Package*", "*\.git\*",'*\$RECYCLE.BIN\*',"*\System Volume Information\*",
     "*\dxbuildgenerator\packages\*","*\_Resharper\*","*\ScreenCapture\*","*.log","*web_view.html","win_view.html",
     "web_view.jpeg","win_view.jpeg","*\Xpand.DLL\*","*.user","*\.vs\*","*.suo","*\bin\*","*\obj\*","*.docstates","*teamcity*","*.gitattributes","*.gitmodules","*.gitignore"|
     where{$fullName -like $_}).Length -eq 0
