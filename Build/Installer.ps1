2985a49f71390bfc25ff1c77cccb0d0c268f10a5
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-20 10:10:15 +0200

    XpandPosh 1.3.20

--- Build/Installer.ps1
+++ Build/Installer.ps1
@@ -23,7 +23,8 @@ Get-ChildItem -Path ".\Xpand.DLL" -Include "*.*" | Where-Object{
     (("*.dll","*.exe","*.config","*.pdb"|Where-Object{$fullName -like $_}).Length -gt 0) -and ($fullName -notlike "*\Plugins\*")
 } | 
 Copy-Item -Destination "$installerFolder\Xpand.DLL\" -Force
-Compress-7Zip -ArchiveFileName $packageFolder\Xpand-lib-$DXVersion.zip -path $installerFolder\Xpand.DLL
+Compress-Archive -DestinationPath $packageFolder\Xpand-lib-$DXVersion.zip -path $installerFolder\Xpand.DLL
+"Creating Xpand-lib-$DXVersion.zip"
 
 Copy-Item "$XpandFolder\Xpand.DLL\Plugins\Xpand.VSIX.vsix" "$packageFolder\Xpand.VSIX-$DXVersion.vsix"
 Copy-Item "$XpandFolder\Xpand.DLL\Plugins\Xpand.VSIX.vsix" "$installerfolder\Xpand.VSIX-$DXVersion.vsix"
@@ -37,7 +38,8 @@ Get-ChildItem $XpandFolder -recurse -Include "*.*" |Where-Object{
     Where-Object{$fullName -like $_}).Length -eq 0)
 } | ForEach-Object {CloneItem $_ -TargetDir $sourceFolder -SourceDir $XpandFolder  }
 Remove-Item "$sourceFolder\build" -Recurse -Force 
-Compress-7Zip -ArchiveFileName "$installerFolder\Source.zip" -path $sourceFolder 
+Compress-Archive -Path $sourceFolder  -DestinationPath "$installerFolder\Source.zip"
+"Creating source.zip"
 Remove-Item $sourceFolder -Force -Recurse
 
 Copy-Item "$installerFolder\Source.zip" -Destination "$packageFolder\Xpand-Source-$DXVersion.zip"
